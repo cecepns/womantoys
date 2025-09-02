7667a6532f39e8560b8c2855a0d7d6dc226c33c9
Muhamad Rifky <muhamad.rifky.dev@gmail.com>
2025-09-02 08:53:34 +0700

    Remove CarouselSlideSeeder from DatabaseSeeder to streamline seeding process

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminSeeder::class,
-            CarouselSlideSeeder::class,
             MainCategorySeeder::class,
             CategorySeeder::class,
             BankAccountSeeder::class,
